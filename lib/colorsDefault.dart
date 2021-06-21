4046b7ce24a167cb9f0b6601a2e253ac61a6d2bc
matheus1714 <matheussilvamartins1714@gmail.com>
2021-06-21 18:11:44 -0300

    set colors default

---
+++ lib/colorsDefault.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DefaultColors {
+  static const Color primaryVioletColor = Color(0xff340794);
+  static const Color secondaryVioletColor = Color(0xff340794);
+
+  static const Color primaryYellowColor = Color(0xff948C16);
+  static const Color secondaryYellowColor = Color(0xffE0D538);
+  static const Color tertiaryYellowColor = Color(0xffFFF459);
+  
+  static const Color whiteColor = Color(0xffffffff);
+  static const Color blackColor = Color(0xff000000);
+}
--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'colorsDefault.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -11,134 +12,158 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.only(left: 30, right: 30, top: 30),
-          child: Column(
-            children: [
-              Container(
-                alignment: Alignment.topCenter,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Text(
-                    'Dingo Music',
-                    style: TextStyle(fontSize: 50),
-                  ),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                  // borderRadius: BorderRadius.all(Radius.circular(100)),
-                  shape: BoxShape.circle,
-                ),
-                height: 200,
-                width: 200,
-                alignment: Alignment.center,
-                child: FittedBox(
-                  fit: BoxFit.fitWidth,
-                  child: Container(
-                    alignment: Alignment.center,
-                    child: Image.asset(
-                      'assets/images/principal_icon.png',
-                      height: 100,
-                      width: 100,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              const Color(0xFF3366FF),
+              const Color(0xFF00CCFF),
+            ],
+            begin: const FractionalOffset(0.0, 0.0),
+            end: const FractionalOffset(1.0, 0.0),
+            stops: [0.0, 1.0],
+          ),
+        ),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.only(left: 30, right: 30, top: 30),
+            child: Column(
+              children: [
+                Container(
+                  alignment: Alignment.topCenter,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Text(
+                      'Dingo Music',
+                      style: TextStyle(fontSize: 50),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                  child: ConstrainedBox(
-                constraints: BoxConstraints(maxWidth: 300),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Nome do Artista',
+                SizedBox(height: 30),
+                Container(
+                  decoration: BoxDecoration(
+                    color: DefaultColors.secondaryVioletColor,
+                    // borderRadius: BorderRadius.all(Radius.circular(100)),
+                    shape: BoxShape.circle,
+                  ),
+                  height: 200,
+                  width: 200,
+                  alignment: Alignment.center,
+                  child: FittedBox(
+                    fit: BoxFit.fitWidth,
+                    child: Container(
+                      alignment: Alignment.center,
+                      child: Image.asset(
+                        'assets/images/principal_icon.png',
+                        height: 100,
+                        width: 100,
+                      ),
+                    ),
                   ),
                 ),
-              )),
-              SizedBox(height: 30),
-              Container(
-                child: ConstrainedBox(
+                SizedBox(height: 30),
+                Container(
+                    child: ConstrainedBox(
                   constraints: BoxConstraints(maxWidth: 300),
                   child: TextField(
                     decoration: InputDecoration(
                       border: OutlineInputBorder(),
-                      labelText: 'Nome da Música',
+                      labelText: 'Nome do Artista',
+                    ),
+                  ),
+                )),
+                SizedBox(height: 30),
+                Container(
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 300),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Nome da Música',
+                      ),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 300.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.purple,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      textStyle:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  onPressed: () {},
-                  child: Text('Buscar Letra'),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 300.0,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                      primary: Colors.purple,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-                      textStyle:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                  onPressed: () {},
-                  child: Text('Buscar no YouTube'),
-                ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.purple,
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(top: 10, bottom: 10),
-                  child: Column(
-                    children: [
-                      Text('Nome da Música', style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 20
-                      ),),
-                      SizedBox(height: 10),
-                      Text('Nome do Cantor ou Banda', style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 15
-                      ),),
-                      SizedBox(height: 10),
-                      Container(
-                        child: Text('dddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss\ndddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss\ndddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss', style: TextStyle(
-                        color: Colors.white,
-                      ),),
-                      ),
-                    ],
+                SizedBox(height: 30),
+                Container(
+                  width: 300.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: DefaultColors.secondaryVioletColor,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    onPressed: () {},
+                    child: Text('Buscar Letra'),
                   ),
                 ),
-              ),
-              SizedBox(height: 30),
-              Container(
-                width: 300,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.purple
+                SizedBox(height: 30),
+                Container(
+                  width: 300.0,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: DefaultColors.secondaryVioletColor,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    onPressed: () {},
+                    child: Text('Buscar no YouTube'),
+                  ),
                 ),
-                alignment: Alignment.center,
-                child: Text('@copyright Dingo Music', style: TextStyle(color: Colors.white, fontSize: 15, fontWeight: FontWeight.bold),),
-              )
-            ],
+                SizedBox(height: 30),
+                Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    color: DefaultColors.secondaryVioletColor,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(top: 10, bottom: 10),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Nome da Música',
+                          style: TextStyle(
+                              color: DefaultColors.whiteColor, fontSize: 20),
+                        ),
+                        SizedBox(height: 10),
+                        Text(
+                          'Nome do Cantor ou Banda',
+                          style: TextStyle(
+                              color: DefaultColors.whiteColor, fontSize: 15),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                          child: Text(
+                            'dddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss\ndddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss\ndddddddddddddddd\nqaqqqqqqqqqqqqqqqqqqqqqqqq\nsssssssssssssssssssssss\nddddddddddddddddddddddd\neeeeeeeeeeeeeeeeeeeeee\ndddddddddddddddddddd\nddddddddddddddddddddd\ndddddddddddddddddddddda\neeeeeeeeeeeeeeeeeeeeeeeeeeeee\nssssssssssssssssssssssssss\nsssssssssssssssssssssss\nsssssssssssssssssss',
+                            style: TextStyle(
+                              color: DefaultColors.whiteColor,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Container(
+                  width: 300,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    color: DefaultColors.secondaryVioletColor,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    '@copyright Dingo Music',
+                    style: TextStyle(
+                        color: DefaultColors.whiteColor,
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
